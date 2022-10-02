a14abdc050bfa1387db6760f0fe8cca1410d49b3
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-02 05:30:51 +0100

    Started hangman image

--- run.py
+++ run.py
@@ -7,3 +7,9 @@ def clear():
     This function clears the terminal using the os import
     """
     os.system("clear")
+
+
+def update_hangman_image(values):
+    print()
+    print("\t +--------+")
+    print("\t |       | |")
